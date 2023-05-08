d1f062a9912cfa40b2d53bfd3a64e08f0fd27211
MarceloCostaMiklos <marcelocostatrt@gmail.com>
2023-05-08 08:20:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ac4pokemon/dados_pokemon.kt
@@ -0,0 +1,11 @@
+package com.example.ac4pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dados_pokemon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dados_pokemon)
+    }
+}
---
+++ app/src/main/java/com/example/ac4pokemon/pesquisar_pokemon.kt
@@ -0,0 +1,11 @@
+package com.example.ac4pokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pesquisar_pokemon : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pesquisar_pokemon)
+    }
+}
